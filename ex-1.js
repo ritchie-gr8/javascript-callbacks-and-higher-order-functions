d48cb1a02e0444bc38f088a76dff1260e2a45766
Pom <kasiphit@gmail.com>
2025-03-04 14:59:12 +0700

    feat:complete callbacks and higher order functions exercise

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,20 @@
 
 function forEach(array, operation) {
   // Start coding here
+  for (let salary of array) {  
+  operation(salary);
+  }
 }
+
+function addSalary(salary) {
+  newEmployeeSalaries.push(salary + 5000);
+}
+
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
 // Using `forEach` function here
+forEach(employeeSalaries, addSalary)
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,22 @@
 //Exercise #2: At Least Five Function
 
-function atLeastFive(array, operation) {
+function atLeastFive(array, operation, classroomNumber) {
   // Start coding here
+  let pass = 0;
+  for (let score of array) {
+    if (score > 70) {
+      pass++;
+    }
+  }
+  return operation(pass, classroomNumber);
+}
+
+function checkPass(pass, classroomNumber) {
+  if (pass >= 5) {
+    return `นักเรียนห้องที่ ${classroomNumber} ผ่านเกณฑ์✅`;
+  } else {
+    return `นักเรียนห้องที่ ${classroomNumber} ไม่ผ่านเกณฑ์ ❌`;
+  }
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +25,10 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, checkPass, 1);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, checkPass, 2);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, checkPass, 3);
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
